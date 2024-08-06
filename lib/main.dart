ac625baedde24556ee5d9cdbe377bffae3d322c2
Abel Ekode <abelekode@gmail.com>
2024-08-06 12:40:06 +0300

    new commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// i need to import a helper library
+//from flutter to get content on the screen
+import 'package:flutter/material.dart';
+
+//define the main function
+void main() {
+//create a new text widget to show shome widget on the screen
+  var app = MaterialApp(
+    home: Text('Hi there'),
+    );
+//take that widget and get it on the screen
+}
